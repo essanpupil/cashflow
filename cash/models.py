96fa1df66dae62fe40ffc5a25abe9d57044fa5d8
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-11-07 08:44:29 +0700

    write cashflow model & makemigrations

--- cash/models.py
+++ cash/models.py
@@ -1,3 +1,25 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class ProofOfTransaction(models.Model):
+    proof_file = models.FileField()
+
+
+class CashFlow(models.Model):
+    TRANSACTION_TYPE_CHOICES = (
+        ('debit',  'Debit'),
+        ('credit', 'Credit')
+    )
+    datetime = models.DateTimeField(default=timezone.now)
+    transaction_proof = models.ForeignKey(ProofOfTransaction, null=True)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    transaction_type = models.CharField(
+        choices=TRANSACTION_TYPE_CHOICES, default='credit',
+        null=False, blank=False, max_length=6
+    )
+    transaction_value = models.PositiveIntegerField()
+    balance = models.IntegerField()
+
+    def __str__(self):
+        return "{0}: {1}".format(self.description, self.transaction_value)
