2be748459d98f904593f5dd9d8f4362b0cc0330a
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-11-27 21:47:14 +0700

    refactor test module & add new unit test for activity list page

--- cash/test_views.py
+++ cash/test_views.py
@@ -2,18 +2,29 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 
 
-class IndexTest(TestCase):
+class CashTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+
+class IndexTest(CashTestCase):
 
     def test_index(self):
-        client = Client()
-        response = client.get(reverse('cash:index'))
+        response = self.client.get(reverse('cash:index'))
         self.assertEqual(200, response.status_code)
         self.assertIn(b'<title>This is the cash application</title>', response.content)
 
 
-class CashActivityTest(TestCase):
+class CashActivityTest(CashTestCase):
 
     def test_get_cash_activity(self):
-        client = Client()
-        response = client.get(reverse('cash:new_activity'))
+        response = self.client.get(reverse('cash:new_activity'))
+        self.assertEqual(200, response.status_code)
+
+
+class ActivityListTest(CashTestCase):
+
+    def test_get_activity_list(self):
+        response = self.client.get(reverse('cash:activity_list'))
         self.assertEqual(200, response.status_code)
