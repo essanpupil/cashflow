b46a98822348c1a8f6125a418bb2696ec043216e
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-11-27 17:45:46 +0700

    view & (empty) template for cash activity input page,
    passed cash  unit test but failed functional test

--- cash/views.py
+++ cash/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import CreateView
+
+from . import models
 
 
 def index(request):
     return render(request, 'cash/index.html')
+
+
+class NewActivity(CreateView):
+    model = models.Activity
+    fields = ['description', 'time', 'activity_type', 'value']
+    template_name = 'cash/new_cash_activity.html'
