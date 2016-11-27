f28496a3a88f76ebbb5af6768ccfde45d0a3e5e0
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-11-27 23:16:43 +0700

    fix model, increase max_length to store value 'credit'

--- cash/models.py
+++ cash/models.py
@@ -10,5 +10,5 @@ class Activity(models.Model):
     )
     description = models.CharField(max_length=255, null=False, blank=False)
     value = models.PositiveIntegerField()
-    activity_type = models.CharField(choices=ACTIVITY_TYPE_CHOICES, default='credit', max_length=5)
+    activity_type = models.CharField(choices=ACTIVITY_TYPE_CHOICES, default='credit', max_length=6)
     time = models.DateTimeField(default=datetime.now)
