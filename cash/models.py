46c5f7f135fe6d84d55de3af3136adb1516b9c1e
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-11-27 17:38:59 +0700

    prepare model to store cash activity

--- cash/models.py
+++ cash/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    ACTIVITY_TYPE_CHOICES = (
+        ('credit', 'Credit'),
+        ('debit', 'Debit')
+    )
+    description = models.CharField(max_length=255, null=False, blank=False)
+    value = models.PositiveIntegerField()
+    activity_type = models.CharField(choices=ACTIVITY_TYPE_CHOICES, default='credit', max_length=5)
+    time = models.DateTimeField(default=datetime.now)
